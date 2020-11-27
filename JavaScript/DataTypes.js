5993394ceaf8827ba91b9ba8e4a1bc5cf9e797a4
Fan Guo <guofx@dukes.jmu.edu>
2020-11-26 21:37:08 -0500

    settting up my project

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,19 @@
+var aNumber = 1;
+
+var aString = "Hello World";
+
+var aUndefined;
+
+var aBoolean = false;
+
+var aBigInt;
+
+var aSymbol;
+
+var aObject = {
+  name: "I'm a object",
+};
+
+var aFunction = function () {
+  return "I'm a function";
+};
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # JavaScriptNotes
 
-I will go over all the concept in plain JavaScript as my fututer reference
+I will go over all the concepts in pure JavaScript as my future reference
 
+我会在这里整理我学习过程中的所有看过觉得有用的文档，视频链接，以及实例
 
 Bye
