d3f17bdf1264e5cbfe479d273c5fa68f285911f0
Fan Guo <guofx@dukes.jmu.edu>
2020-12-03 23:56:40 -0500

    Closure

---
+++ JavaScript/Closure.js
@@ -0,0 +1,41 @@
+// Closure
+// https://github.com/lgwebdream/FE-Interview/issues/17
+
+// What is Closure？
+// Js中有对Scope的定义和其他的语言不太一样， lexical scope,
+// 这就意味着在一个特定的scope中，里面会自动获得 access 外部的所有 props,
+// 但是外部无法对内部的 props 进行访问
+
+let outer = 10;
+
+function add() {
+  let inner = 20;
+  console.log();
+  return inner + outer; // outer 可以直接被调用
+}
+
+console.log(add()); // return 30
+//console.log(inner); // 报错， inner is undefined
+
+// so what is closure?
+// closure is a special scope, that holds variable the function gonna use
+// and makes them available outside
+
+// 个人理解： 闭包是一种特殊区间，这个区间会包含某个函数将要用到的变量， 并让这些变量在其他地方也可以使用
+// 变量是独立存在的， 每一个单独的闭包会有自己的 closure.
+// 形成的条件就是你有一个 functionA
+// 并且该functionA 会返回另外一个functionB, functionB 中需要用到 functionA 中的变量
+
+let functionA;
+
+function OuterFunction() {
+  let innerData = "Hi";
+  return function (...data) {
+    console.log(innerData);
+    console.log(...data);
+  };
+}
+
+functionA = OuterFunction();
+functionA("called", "haha");
+// innerData still accessible here, innerData is inside a closure
--- JavaScript/Throttling.js
+++ JavaScript/Throttling.js
@@ -3,13 +3,13 @@
 
 // 需要 button 来更好的展现效果，
 // 点击下面的链接，看看代码吧
-// CodePen : https://codepen.io/fanguo484/pen/eYdJzQd?editors=1011
+// CodePen : https://codepen.io/Bilibiber/pen/eYdJzQd
 
-//Throttling 节流
+//Throttling 节流 里面有注释
 // V1 用的setTimeOut()
-// CodePen:https://codepen.io/fanguo484/pen/poEgWya?editors=1011
+// CodePen:https://codepen.io/Bilibiber/pen/poEgWya
 
 // V2 用的时间戳
-// CodePen:https://codepen.io/fanguo484/pen/NWRxgGO
+// CodePen:https://codepen.io/Bilibiber/pen/NWRxgGO
 
 // 个人觉得时间戳的方式更好一些， 因为用户可以直接得到反馈，不需要delay. 同时我们也有节流的效果
